4bd2b3dcbcaf599a05f5efb4538ead927b8b96df
michael guldan <michaelguldan@gmail.com>
2015-05-21 16:08:18 -0700

    updates to the linker script to add htoprc and bash_functions

--- DFlink.sh
+++ DFlink.sh
@@ -8,7 +8,7 @@
 ########## Variables
 dfdir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc gitconfig wgetrc screenrc XCompose ssh/config"    # list of files/folders to symlink in homedir
+files="bashrc vimrc gitconfig wgetrc screenrc XCompose ssh/config bash_functions config/htop/htoprc"    # list of files/folders to symlink in homedir
 ##########
 
 # do all the work from the dotfiles dir

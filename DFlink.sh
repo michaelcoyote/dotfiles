06bac221bb700427c2b5fb624e91eea0873b6f60
michael guldan <michaelguldan@gmail.com>
2017-01-18 15:52:36 -0800

    Linting and cleanup of DFlink.sh

--- DFlink.sh
+++ DFlink.sh
@@ -17,24 +17,24 @@ mkdir -p $olddir
 
 # move any existing dotfiles in homedir to backup dir, then create symlinks
 for file in $files; do
-    if [ -e $dfdir/$file ] # make sure source file exists
+    if [ -e "$dfdir"/"$file" ] # make sure source file exists
     then
         # Link back to the dotfile
-        if [ -L ~/.$file ] #check for existing symlink
+        if [ -L ~/."$file" ] #check for existing symlink
         then
             echo "Link exists for ~./$file"
         elif [ "$(stat -c %h -- "$file")" -gt 1 ]
         then
             echo "~/.$file has more than one name, check for hard links."
         else
-            if [ -e ~/.$file ] # if .file exists, back up
+            if [ -e ~/."$file" ] # if .file exists, back up
             then    
-                mkdir -p $olddir # create dotfiles_old in homedir
+                mkdir -p "$olddir" # create dotfiles_old in homedir
                 echo "Move ~/.$file to $olddir"
-                mv ~/.$file $olddir
+                mv ~/."$file" "$olddir"
             fi
             echo "Creating symlink to ~/.$file"
-            ln -s $dfdir/$file ~/.$file
+            ln -s "$dfdir"/"$file" ~/."$file"
         fi
     else
         echo "File $file is missing from $dfdir"  

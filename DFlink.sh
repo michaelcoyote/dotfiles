79eb4f3d3d51931e7b70153271a6ccbd5a7d85b6
michael guldan <michaelguldan@gmail.com>
2024-07-28 02:33:12 -0700

    Allow install script to better support macos

--- DFlink.sh
+++ DFlink.sh
@@ -10,9 +10,20 @@ dfdir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="bashrc vimrc gitconfig wgetrc screenrc XCompose ssh/config bash_profile
-bash_functions config/htop/htoprc config/parcellite/parcelliterc alias psqlrc
+bash_functions config/htop/htoprc alias psqlrc
 ctags tmux.conf bash_${HOSTNAME} bash_supporttunnel"
 ##########
+
+# function to support macos & linux w/o editing
+function linkcount()
+{
+    if [[ $OSTYPE == darwin* ]];
+        then
+            stat -f %l -- "$1";
+        else
+            stat -c %h -- "$1";
+    fi
+}
 
 # do all the work from the dotfiles dir
 cd $dfdir || exit
@@ -26,12 +37,11 @@ for file in $files; do
         if [ -L ~/."$file" ] #check for existing symlink
         then
             echo "Link exists for ~./$file"
-        # elif [ "$(stat -f %l -- "$file")" -gt 1 ]
-        elif [ "$(stat -c %h -- "$file")" -gt 1 ]
+        elif [ "$(linkcount "$HOME"/."$file")" -gt 1 ]
         then
             echo "$HOME/.$file has more than one name, check for hard links."
         else
-            if [ -e ~/."$file" ] # if .file exists, back up
+        if [ -e ~/."$file" ] # if .file exists, back up
             then
                 mkdir -p "$olddir" # create dotfiles_old in homedir
                 echo "Move ~/.$file to $olddir"
